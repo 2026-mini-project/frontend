15df3da5d6cb81ec001bc6bc653e7cb016c64d27
prosirea2501@gmail.com <prosirea2501@gmail.com>
2026-06-15 15:31:29 +0900

    chore: declarations.d.ts 추가

---
+++ declarations.d.ts
@@ -0,0 +1,23 @@
+declare global {
+    type APIError = {
+        "message": string
+    };
+
+    type APIStatus = {
+        "running": boolean
+    };
+
+    type APIUser = {
+        "id": string, // SessionId임
+        "name": string,
+    };
+
+    type APIRoom = {
+        "id": string,
+        "name": string,
+        "owner": string, // SessionId임
+        "full": boolean // 방이 꽉 찼는지 여부
+    };
+}
+
+export { };
