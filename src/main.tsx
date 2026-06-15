864fa783b455f64305c08ee105d520e8cfe146de
prosirea2501@gmail.com <prosirea2501@gmail.com>
2026-06-15 15:01:41 +0900

    chore: 라우팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
-import Page from './App';
+import { default as MainPage } from './App';
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <Routes>
-            <Route element={<Page />} path="/" />
+            <Route element={<MainPage />} path="/" />
         </Routes>
     </BrowserRouter>,
 );
