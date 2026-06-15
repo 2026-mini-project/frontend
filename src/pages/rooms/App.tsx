1756f4e9cfe72cf6f72326e82a15e863c3357ee9
prosirea2501@gmail.com <prosirea2501@gmail.com>
2026-06-15 17:14:43 +0900

    chore: 나중에 만들어야지

--- src/pages/rooms/App.tsx
+++ src/pages/rooms/App.tsx
@@ -1,6 +1,20 @@
+import { useEffect, useState } from 'react';
+import Transition from '../../components/transition';
+import css from './App.module.css';
+
 export default function Page() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        (async () => {
+            await new Promise(r => setTimeout(r, 1000));
+            setLoading(false);
+        })();
+    }, []);
+
     return <>
-        <div>
+        <Transition hide={loading} />
+        <div className={css.container}>
 
         </div>
     </>;
