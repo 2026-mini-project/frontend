40be7596ebb174c2726304307cc6ea0c3b716679
prosirea2501@gmail.com <prosirea2501@gmail.com>
2026-06-15 16:26:24 +0900

    chore: 로딩 돌아가는거 삭제

--- src/components/transition/index.tsx
+++ src/components/transition/index.tsx
@@ -1,28 +1,18 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import css from './styles.module.css';
-import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useState } from 'react';
 import { animated, easings, useSpringValue } from '@react-spring/web';
 
 type Props = {
     "hide": boolean,
-    "loading"?: boolean,
     "onAnimationEnd"?: () => any
 }
 
-const AnimatedFA = animated(FontAwesomeIcon);
-
 export default function Transition(props: Props) {
-    const { hide, loading, onAnimationEnd } = props;
+    const { hide, onAnimationEnd } = props;
     const [doNotRender, setDoNotRender] = useState(false);
 
     const containerOpacity = useSpringValue(hide ? 1 : 0, {
-        "config": {
-            "duration": 480,
-            "easing": easings.easeInOutCubic
-        }
-    });
-    const loadingOpacity = useSpringValue(loading ? 1 : 0, {
         "config": {
             "duration": 480,
             "easing": easings.easeInOutCubic
@@ -43,28 +33,10 @@ export default function Transition(props: Props) {
         })();
     }, [hide]);
 
-    useEffect(() => {
-        (async () => {
-            if (loading) {
-                await loadingOpacity.start(1);
-            } else {
-                await loadingOpacity.start(0);
-            }
-
-            onAnimationEnd?.();
-        })();
-    }, [loading]);
-
     if (doNotRender) return null;
 
     return <animated.div
         className={css.container}
         style={{ "opacity": containerOpacity }}
-    >
-        <AnimatedFA
-            icon={faCircleNotch}
-            spin
-            style={{ "opacity": loadingOpacity }}
-        />
-    </animated.div>
+    />;
 }
