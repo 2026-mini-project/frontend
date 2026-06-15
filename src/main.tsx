1175ee7044960d3bf032f16c75f6c890fff87df1
prosirea2501@gmail.com <prosirea2501@gmail.com>
2026-06-15 15:03:41 +0900

    chore: 폴더 구조 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
-import { default as MainPage } from './App';
+import { default as MainPage } from './pages/App';
+import { default as GamePage } from './pages/game/App';
+import { default as RoomsPage } from './pages/rooms/App';
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <Routes>
             <Route element={<MainPage />} path="/" />
+            <Route element={<GamePage />} path="/game" />
+            <Route element={<RoomsPage />} path="/rooms" />
         </Routes>
     </BrowserRouter>,
 );
