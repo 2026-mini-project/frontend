fd4afa4966972efb426854a713e04993d67e8161
prosirea2501@gmail.com <prosirea2501@gmail.com>
2026-06-17 16:51:39 +0900

    chore: 방 목록 불러오기

--- src/pages/rooms/App.tsx
+++ src/pages/rooms/App.tsx
@@ -1,13 +1,27 @@
 import { useEffect, useState } from 'react';
 import Transition from '../../components/transition';
 import css from './App.module.css';
+import REST from '../../modules/rest';
 
 export default function Page() {
     const [loading, setLoading] = useState(true);
+    const [rooms, setRooms] = useState<APIRoom[]>([]);
 
     useEffect(() => {
         (async () => {
-            await new Promise(r => setTimeout(r, 1000));
+            const r = await REST<APIRoom[]>("/rooms");
+            if (!r.success) {
+                if (r.status === 404) {
+                    setLoading(false);
+                    return;
+                }
+
+                alert(r.data.message);
+                return window.history.back();
+            }
+
+            setRooms(r.data);
+
             setLoading(false);
         })();
     }, []);
@@ -15,7 +29,7 @@ export default function Page() {
     return <>
         <Transition hide={loading} />
         <div className={css.container}>
-
+            
         </div>
     </>;
 }
