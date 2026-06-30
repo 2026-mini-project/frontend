3d2cfdb690142e153770f193bbf74c7905ad9f5c
prosirea2501@gmail.com <prosirea2501@gmail.com>
2026-06-30 15:44:49 +0900

    fix: 라우팅 수정

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import { default as MainPage } from './pages/App';
-import { default as GamePage } from './pages/game/App';
 import { default as RoomsPage } from './pages/rooms/App';
 import { default as RoomPage } from './pages/rooms/id/App';
 
@@ -10,7 +9,6 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <Routes>
             <Route element={<MainPage />} path="/" />
-            <Route element={<GamePage />} path="/game" />
             <Route element={<RoomsPage />} path="/rooms" />
             <Route element={<RoomPage />} path="/rooms/:id" />
         </Routes>
